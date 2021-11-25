syberia-project/platform_vendor_syberia

Add back turbo prebuilt
Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I6dce48eb31909102403d0b4e6b9fd43511a381c5
Signed-off-by: DennySPb <dennyspb@gmail.com>

config/common.mk
@@ -87,7 +87,8 @@ PRODUCT_COPY_FILES += \
     vendor/syberia/config/permissions/privapp-permissions-syberia.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-syberia.xml \
     vendor/syberia/config/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-custom.xml \
     vendor/syberia/prebuilt/google/etc/sysconfig/pixel_experience_2020.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_experience_2020.xml \
-    vendor/syberia/prebuilt/google/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+    vendor/syberia/prebuilt/google/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
+    vendor/syberia/prebuilt/google/etc/permissions/privapp-permissions-googleapps-turbo.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-googleapps-turbo.xml
 
 # Fonts
 PRODUCT_COPY_FILES += \

GioKas93/vendor_org

config/common.mk
@@ -69,6 +69,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Default and google apps privapp permissions
 PRODUCT_COPY_FILES += \
     vendor/orgasmic/prebuilt/google/etc/sysconfig/pixel_experience_2020.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_experience_2020.xml
+    vendor/orgasmic/prebuilt/google/etc/permissions/privapp-permissions-googleapps-turbo.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-googleapps-turbo.xml
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \